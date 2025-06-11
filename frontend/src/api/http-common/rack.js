1b495f8909fd7eb41230b7adfd9e4d0316b5c15a
claubells <claudia.villa@usach.cl>
2025-06-11 03:05:07 -0400

    ysssl

--- frontend/src/api/http-common/rack.js
+++ frontend/src/api/http-common/rack.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const host = import.meta.env.VITE_API_GATEWAY_HOST;
-const port = import.meta.env.VITE_API_GATEWAY_PORT;
+const host = import.meta.env.VITE_API_BACKEND_SERVER;
+const port = import.meta.env.VITE_API_BACKEND_PORT;
 
 export default axios.create({
   baseURL: `http://${host}:${port}/api/rack`,
--- frontend/src/api/http-common/rates.js
+++ frontend/src/api/http-common/rates.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const host = import.meta.env.VITE_RATES_BACKEND_HOST;
-const port = import.meta.env.VITE_RATES_BACKEND_PORT;
+const host = import.meta.env.VITE_API_BACKEND_SERVER;
+const port = import.meta.env.VITE_API_BACKEND_PORT;
 
 export default axios.create({
   baseURL: `http://${host}:${port}`,
