74778e5102ed8daac5e3339ff845d10de954bea2
claubells <claudia.villa@usach.cl>
2025-06-10 22:56:47 -0400

    se modifican algunos aprametros del front

--- frontend/src/api/http-common/reservation.js
+++ frontend/src/api/http-common/reservation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const host = import.meta.env.VITE_RESERVATION_BACKEND_HOST;
-const port = import.meta.env.VITE_RESERVATION_BACKEND_PORT;
+const host = import.meta.env.VITE_API_GATEWAY_HOST;
+const port = import.meta.env.VITE_API_GATEWAY_PORT;
 
 export default axios.create({
   baseURL: `http://${host}:${port}/api`,
